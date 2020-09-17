e1cb082edd8d66671566cff70a7b9faa52cf86d2
link <1020535012@qq.com>
2020-09-17 17:30:45 +0800

    '1'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React ,{Component} from "react";
+import { Button } from 'antd';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                App描述
+                <Button>点击按钮</Button>
+            </div>
+        );
+    }
+}
